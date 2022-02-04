deef191d37fb3605e4734d41e333b1f35b441bc3
David Tran <frozenpho@gmail.com>
2022-02-04 13:36:20 -0700

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <p>boner</p>
+      </div>
+    )
+  }
+}
+
+export default App;
