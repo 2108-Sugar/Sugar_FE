1db7e085381d8f37cb2eaf0ee97cd4db64fbfd13
Tony Hsu <tdh425@gmail.com>
2022-02-04 14:23:58 -0700

    Create sample data

---
+++ src/Components/SampleData/sample-data.js
@@ -0,0 +1,69 @@
+const data = [
+  {
+    community_id: '1',
+    lender_id: '1',
+    lender_name: 'David',
+    ticket_id: '1',
+    item_name: 'Hammer',
+    category: 'Tools',
+    borrow_date: '02/10/22',
+    return_date: '02/11/22',
+    post_date: '02/05/22',
+    status: '0',
+    request_details: 'Need a claw hammer for clawing around 11pm Feb 10'
+  },
+  {
+    community_id: '1',
+    lender_id: '2',
+    lender_name: 'Tony',
+    ticket_id: '2',
+    item_name: 'Shovel',
+    category: 'Tools',
+    borrow_date: '02/9/22',
+    return_date: '02/10/22',
+    post_date: '02/05/22',
+    status: '0',
+    request_details: 'Need a shovel for stuff around 11pm Feb 9.  Please don\'t ask why.  It will be returned the same condition as borrowed'
+  },
+  {
+    community_id: '1',
+    lender_id: '3',
+    lender_name: 'Didi',
+    ticket_id: '3',
+    item_name: 'Crowbar',
+    category: 'Tools',
+    borrow_date: '02/17/22',
+    return_date: '02/20/22',
+    post_date: '02/05/22',
+    status: '0',
+    request_details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    community_id: '1',
+    lender_id: '4',
+    lender_name: 'Billy',
+    ticket_id: '4',
+    item_name: 'Fast Car',
+    category: 'Automotive',
+    borrow_date: '02/13/22',
+    return_date: '02/14/22',
+    post_date: '02/05/22',
+    status: '0',
+    request_details: 'Need a really fast car for a few hours.  Need to make a withdrawl at the bank'
+  },
+  {
+    community_id: '1',
+    lender_id: '5',
+    lender_name: 'Christian',
+    ticket_id: '5',
+    item_name: 'Ski Mask',
+    category: 'Clothing',
+    borrow_date: '02/13/22',
+    return_date: '02/14/22',
+    post_date: '02/05/22',
+    status: '0',
+    request_details: 'Just for a few hours.  Banks are cold Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis'
+  },
+]
+
+export default data
