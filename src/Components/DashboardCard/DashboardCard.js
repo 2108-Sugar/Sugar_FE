06232d464bf0c69e4084592d946527de2d42a307
Tony Hsu <tdh425@gmail.com>
2022-02-07 10:08:34 -0700

    Add styling for bulletin board format

--- src/Components/DashboardCard/DashboardCard.js
+++ src/Components/DashboardCard/DashboardCard.js
@@ -5,7 +5,7 @@ import pin from '../pin2.png'
 const DashboardCard = ({ lenderName, category, itemName, requestDetails, borrowDate, post_date }) => {
   return (
     <div className='request-card'>
-      <img className='pin-img' src={pin} alt='pin' />
+      <img className='pin-img' src={pin} alt='pin' width='90px' height='50px' />
       <div className='top-row'>
         <p className='name'>{lenderName}</p>
         <p>Requested on:  {post_date}</p>
