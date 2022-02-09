7f933ea10f8140bc37bb3e8d4dc3bdc05d2cc718
David Tran <frozenpho@gmail.com>
2022-02-09 14:10:30 -0700

    Create containers for columns

--- src/Components/MyStuffContainer/MyStuffContainer.js
+++ src/Components/MyStuffContainer/MyStuffContainer.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './MyStuffContainer.css'
+import DashboardCard from '../DashboardCard/DashboardCard';
 
 
 const MyStuffContainer = () => {
-  return <div>This is all your borrowed and loaned shit</div>;
+  return (
+    <div className='column-container'>
+      <div className='loaned-items-container'>
+
+      </div>
+      <div className='requested-items-container'>
+
+      </div>
+    </div>
+  )
 }
 
 export default MyStuffContainer;
