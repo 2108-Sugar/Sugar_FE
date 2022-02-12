0ac02fbe888baadb716047c75eb4651d3b0f5227
Tony Hsu <tdh425@gmail.com>
2022-02-12 16:45:14 -0700

    Add car icon for auto category

--- src/Components/SampleData/Category.js
+++ src/Components/SampleData/Category.js
@@ -2,13 +2,15 @@ import tools from '../Images/tools-icon.png'
 import sports from '../Images/sports.png'
 import kitchen from '../Images/blender.png'
 import clothing from '../Images/hanger.png'
+import auto from '../Images/auto.png'
 
 
 const category = {
   'tools': tools,
   'sporting goods': sports,
   'kitchen': kitchen,
-  'clothing': clothing
+  'clothing': clothing,
+  'automotive': auto
 }
 
 
