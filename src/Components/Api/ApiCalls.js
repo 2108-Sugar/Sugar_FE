4abed1ca1035004e7fef8c0e67c95fe35edc53ce
Tony Hsu <tdh425@gmail.com>
2022-02-11 20:33:45 -0700

    change https back to http

--- src/Components/Api/ApiCalls.js
+++ src/Components/Api/ApiCalls.js
@@ -1,5 +1,5 @@
 const fetchApi = () => {
-  return fetch("https://lendsugar.com/api/v1/users/1/communities/1/requests")
+  return fetch("http://lendsugar.com/api/v1/users/1/communities/1/requests")
   .then(response => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.statusText}`)
